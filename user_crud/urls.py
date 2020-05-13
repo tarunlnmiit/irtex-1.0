3ad56437a2111faa9c95aaff6abf5a9652c9336b
Tarun Gupta <tarungupta.y12@lnmiit.ac.in>
2020-05-14 00:37:37 +0200

    heroku setup

--- user_crud/urls.py
+++ user_crud/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('/', views.hello),
     path('userdata/', views.user_list),
     path('userdata/<str:_id>/', views.user_detail),
-    path('/', views.hello),
 ]
