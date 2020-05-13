31ed36ff8c929a08d348385990749b1b1cba82c4
Tarun Gupta <tarungupta.y12@lnmiit.ac.in>
2020-05-14 00:06:44 +0200

    heroku setup

--- irtex/irtex/settings.py
+++ irtex/irtex/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 # System Packages
 import os
 # 3rd party packages
+import django_heroku
 # local packages
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -141,3 +142,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
