ead02e7e9d781190d540742010964b6492eb08c6
Tarun Gupta <tarungupta.y12@lnmiit.ac.in>
2020-05-14 09:33:45 +0200

    heroku setup

--- irtex/settings.py
+++ irtex/settings.py
@@ -91,14 +91,24 @@ WSGI_APPLICATION = 'irtex.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'djongo',
+#         'NAME': 'heroku_44bw2hgk',
+#         'USER': 'root',
+#         'PASSWORD': 'kewPyz-2fiwma-goxhoc',
+#         'HOST': 'mongodb://root:kewPyz-2fiwma-goxhoc@ds233571.mlab.com:33571/heroku_44bw2hgk',
+#         'PORT': 33571
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'djongo',
-        'NAME': 'heroku_44bw2hgk',
+        'NAME': 'irtex_test',
         'USER': 'root',
-        'PASSWORD': 'kewPyz-2fiwma-goxhoc',
-        'HOST': 'mongodb://root:kewPyz-2fiwma-goxhoc@ds233571.mlab.com:33571/heroku_44bw2hgk',
-        'PORT': 33571
+        'PASSWORD': 'Fajcon-witgob-2nizki',
+        'HOST': 'mongodb+srv://root:Fajcon-witgob-2nizki@cluster-irtex-lzpoh.mongodb.net/test?retryWrites=true&w=majority',
     }
 }
 
