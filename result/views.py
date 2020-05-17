e5a800b0c00fa1b93acd8d9dd3ae6c911e9228c4
Tarun Gupta <tarungupta.y12@lnmiit.ac.in>
2020-05-17 20:34:33 +0200

    compute region based similarity based on query image

--- result/views.py
+++ result/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
 from django.http import JsonResponse
+from django.conf import settings
+
+from upload.models import QueryImage
+from region_based_descriptor.RBSDescriptor import RBSDescriptor
+
+import cv2
+import os
+import traceback
 
 
 def getResults(request, _id):
-    print(_id)
+    try:
+        image_instance = QueryImage.objects.get(_id=_id)
+        media_path = os.path.join(settings.BASE_DIR, 'media')
+        image_path = os.path.join(media_path, str(image_instance.file))
+
+        rbsd = RBSDescriptor()
+        img_array = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
+        img_array = rbsd.image_preprocessing(img_array)
+
+        q_moment = rbsd.zernike_moments(img_array)
+        sim = rbsd.similarity(q_moment)
+        print(sim[:10])
+    except Exception as e:
+        print(traceback.print_exc())
     return JsonResponse({
-        'string': 'result'
+        'result': 'result'
     })
