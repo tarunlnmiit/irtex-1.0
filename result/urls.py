75a53c0e56858c11d32d049f6667df311f6863d4
Tarun Gupta <tarungupta.y12@lnmiit.ac.in>
2020-05-18 22:03:50 +0200

    1. query image data url 2. get query image data 3. modification in result url
    (cherry picked from commit 9e005130b5fe72ede69ffae18bea6decb62fcd51)

--- result/urls.py
+++ result/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('rbsd/<str:_id>', views.getRBSDResults),
+    path('<str:_id>', views.getRBSDResults),
     path('cld/<str:_id>', views.getCLDResults),
 ]
