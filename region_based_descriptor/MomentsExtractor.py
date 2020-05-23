81a0ce28382e2b1ed12b7a7ae19168a29dea14de
Tarun Gupta <tarungupta.y12@lnmiit.ac.in>
2020-05-23 18:22:33 +0200

    pickling and unpickling of feature data for RBSDescriptor

--- region_based_descriptor/MomentsExtractor.py
+++ region_based_descriptor/MomentsExtractor.py
@@ -5,8 +5,8 @@ import os
 import cv2
 from tqdm import tqdm
 import mahotas
+import pandas as pd
 import csv
-
 
 
 class ZernikeMoments:
@@ -14,36 +14,40 @@ class ZernikeMoments:
         # store the size of the radius that will be
         # used when computing moments
         self.radius = radius
+
     def describe(self, image):
         # return the Zernike moments for the image
         return mahotas.features.zernike_moments(image, self.radius)
 
+
 desc = ZernikeMoments(16)
 zernike = []
 
-dataFile = os.path.join(settings.BASE_DIR, 'media', 'cifar10')
+dataFile = os.path.join('/Users/tarun/Projects/irtex_env/irtex/media/cifar10/')
 path = os.path.join(dataFile)
 
 data_list = [["file_name", "moments", "label"]]
 
 # Loading the image dataset and converting into zernike moments
 for label in tqdm(os.listdir(path)):
-	for img in tqdm(os.listdir(os.path.join(path, label))):
-		img_array = cv2.imread(os.path.join(path, label, img), cv2.IMREAD_UNCHANGED)
-		img_array = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
-		#img_array = cv2.resize(img_array, (256, 256))
-		#img_array = img_array + np.random.normal(scale=2, size=img_array.shape)
-		moments = desc.describe(img_array)
+    for img in tqdm(os.listdir(os.path.join(path, label))):
+        img_array = cv2.imread(os.path.join(path, label, img), cv2.IMREAD_UNCHANGED)
+        img_array = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
+        # img_array = cv2.resize(img_array, (256, 256))
+        # img_array = img_array + np.random.normal(scale=2, size=img_array.shape)
+        moments = desc.describe(img_array)
 
-		row = [img, moments, label]
-		data_list.append(row)
+        row = [img, moments, label]
+        data_list.append(row)
 
-		zernike.append(moments)
-
+        zernike.append(moments)
 
 # Storing the extracted features into CSV file
-with open('moments.csv', 'w', newline='') as file:
-	writer = csv.writer(file, delimiter=',')
-	writer.writerows(data_list)
+# with open('moments.csv', 'w', newline='') as file:
+#     writer = csv.writer(file, delimiter=',')
+#     writer.writerows(data_list)
+# file.close()
 
-file.close()
+columns = data_list.pop(0)
+df = pd.DataFrame(data_list, columns=columns)
+pd.to_pickle(df, 'moments.pkl')
