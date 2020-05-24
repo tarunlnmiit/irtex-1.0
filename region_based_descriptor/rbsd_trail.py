1b22d244b4f30a86cf08f35e3c5d0524d5894ed4
libinjohn26 <51414928+libinjohn26@users.noreply.github.com>
2020-05-24 20:44:45 +0200

    Command line implementation

--- region_based_descriptor/rbsd_trail.py
+++ region_based_descriptor/rbsd_trail.py
@@ -1,3 +1,6 @@
+# Testing region based shape descriptor on toy dataset
+# python rbsd_trail.py --path "path_to_query_image"
+
 import numpy as np
 import matplotlib.pyplot as plt
 import os
@@ -5,8 +8,7 @@ import cv2
 from tqdm import tqdm
 import mahotas
 from sklearn.metrics.pairwise import cosine_similarity
-from skimage.segmentation import slic, felzenszwalb
-
+import argparse
 
 def takefirst(elem):
     return elem[0]
@@ -22,52 +24,63 @@ class ZernikeMoments:
         return mahotas.features.zernike_moments(image, self.radius)
 
 
-desc = ZernikeMoments(50)
-zernike = []
-images = []
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='RBSD Testing')
 
-dataFile = os.getcwd()
-path = os.path.join(dataFile, 'toy_dataset')
-labels = []
-for label in tqdm(os.listdir(path)):
-    img_array = cv2.imread(os.path.join(path, label), cv2.IMREAD_GRAYSCALE)
-    #img_array = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
-    img_array = cv2.resize(img_array, (100, 100))
+    parser.add_argument('--path', help='path to query image')
+    args = parser.parse_args()
+    path = args.path
 
-    #img_array = img_array + np.random.normal(scale=0.2, size=img_array.shape)
-    #img_array = img_array.astype('float32')
+    desc = ZernikeMoments(50)
+    zernike = []
+    images = []
 
-    #img_array = cv2.filter2D(img_array, -1, kernel)
-    #img_array = cv2.bilateralFilter(img_array, 2, 5, 5)
-    # invert the image and threshold it
-    #img_array = cv2.bitwise_not(img_array)
-    moments = desc.describe(img_array)
-    zernike.append(moments)
-    #print(moments)
+    dataFile = os.getcwd()
+    path_toy = os.path.join(dataFile, 'toy_dataset')
+    labels = []
+    for label in tqdm(os.listdir(path_toy)):
+        img_array = cv2.imread(os.path.join(path_toy, label), cv2.IMREAD_GRAYSCALE)
+        #img_array = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
+        img_array = cv2.resize(img_array, (100, 100))
 
-    labels.append(label)
-    images.append(img_array)
-    #plt.figure()
-    #img_array = slic(img_array, n_segments=100, sigma=2)
-    #plt.imshow(img_array, cmap='gray')  # graph it
-    #plt.show()  # display!
+        #img_array = img_array + np.random.normal(scale=0.2, size=img_array.shape)
+        #img_array = img_array.astype('float32')
 
-imgtest = cv2.imread(os.path.join(path, 'circletest.jpg'), cv2.IMREAD_GRAYSCALE) #cv2.IMREAD_UNCHANGED)
-imgtest = cv2.resize(imgtest, (100, 100))
-test_moments = desc.describe(imgtest)
-test_moments = test_moments.reshape(1, -1)
+        #img_array = cv2.filter2D(img_array, -1, kernel)
+        #img_array = cv2.bilateralFilter(img_array, 2, 5, 5)
+        # invert the image and threshold it
+        #img_array = cv2.bitwise_not(img_array)
+        moments = desc.describe(img_array)
+        zernike.append(moments)
+        #print(moments)
 
-sim_t = []
-sim_t = cosine_similarity(zernike, test_moments)
-#for moment in zernike:
-#    sim = np.linalg.norm((moment - test_moments), ord=1)
-#    sim_t.append(sim)
-result = []
-for i,j in zip(sim_t, labels):
-    result.append([i,j])
+        labels.append(label)
+        images.append(img_array)
+        #plt.figure()
+        #img_array = slic(img_array, n_segments=100, sigma=2)
+        #plt.imshow(img_array, cmap='gray')  # graph it
+        #plt.show()  # display!
 
-result.sort(key=takefirst, reverse=True)
+    if path:
+        imgtest = cv2.imread(path, cv2.IMREAD_GRAYSCALE)  # cv2.IMREAD_UNCHANGED)
+    else:
+        imgtest = cv2.imread(os.path.join(path_toy, 'circletest.jpg'), cv2.IMREAD_GRAYSCALE)  # cv2.IMREAD_UNCHANGED)
 
-for i in result:
-    print(i)
+    imgtest = cv2.resize(imgtest, (100, 100))
+    test_moments = desc.describe(imgtest)
+    test_moments = test_moments.reshape(1, -1)
+
+    zernike = np.array(zernike)
+
+    sim_t = []
+    sim_t = cosine_similarity(zernike, test_moments)
+
+    result = []
+    for i,j in zip(sim_t, labels):
+        result.append([i,j])
+
+    result.sort(key=takefirst, reverse=True)
+
+    for i in result:
+        print(i)
 
