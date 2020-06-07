62a6956d69ca5c4ad2aaf4790d084481624e6dc6
Tarun Gupta <tarungupta.y12@lnmiit.ac.in>
2020-06-07 16:54:43 +0200

    resnet view and combined view updated

--- result/views.py
+++ result/views.py
@@ -10,12 +10,13 @@ from local_feature_descriptor.sift import SIFT, get_similarity_sift
 from segmentation.Segmentation_pascal import extract_features_pascal, get_similarity_segmentation_pascal
 from segmentation.NN_segmentation import segmentation_cifar, get_similarity_segmentation_cifar
 from vgg16_imagenet_features.VGG16FeatureExractor import extract_feature_vgg,get_similarity_vgg
-from resnet20_cifar_10_features.ResNet20FeatureExtractor import extract_feature_resnet,get_similarity_resnet
+from resnet_features.ResNet20FeatureExtractor import extract_feature_resnet, get_similarity_resnet
 
 import cv2
 import json
 import numpy as np
 import os
+import pandas as pd
 import traceback
 from collections import OrderedDict
 from mxnet import image
@@ -37,8 +38,7 @@ def getCombinedResults(request, _id):
             media_path = os.path.join(settings.BASE_DIR, 'media')
             image_path = os.path.join(media_path, str(image_instance.file))
 
-            final_sim = OrderedDict()
-
+            # RBSD
             rbsd = RBSDescriptor(dataset)
             img_array = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
             img_array = rbsd.image_preprocessing(img_array)
@@ -46,6 +46,7 @@ def getCombinedResults(request, _id):
             sim_rbsd = rbsd.similarity(q_moment)
             sim_rbsd.sort(key=lambda x: x['similarity'], reverse=True)
 
+            # CLD
             cld = CLDescriptor()
             img_array = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
             descriptor = cld.compute(img_array).reshape(1, -1)
@@ -54,7 +55,6 @@ def getCombinedResults(request, _id):
             sim_cld.sort(key=lambda x: x['similarity'], reverse=True)
 
             # Segmentation
-
             if dataset == 'cifar':
                 img = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
                 img = cv2.resize(img, (128, 128), interpolation=cv2.INTER_AREA)
@@ -67,6 +67,18 @@ def getCombinedResults(request, _id):
 
             sim_segmentation.sort(key=lambda x: x['similarity'], reverse=True)
 
+            # Resnet
+            if dataset == 'cifar':
+                df = pd.read_pickle(os.path.join(settings.BASE_DIR, 'resnet_features/cifar_resnet_logits.pkl'))
+            elif dataset == 'pascal':
+                df = pd.read_pickle(os.path.join(settings.BASE_DIR, 'resnet_features/deeplab_pascal.pkl'))
+
+            descriptor = df.loc[df['file_name'] == image_instance.file].iloc[0, 1].reshape(1, -1)
+            sim_resnet = get_similarity_resnet(descriptor, dataset)
+
+            sim_resnet.sort(key=lambda x: x['similarity'], reverse=True)
+
+            # Local
             if dataset == 'cifar':
                 features = ['SIFT']
                 sift = SIFT()
@@ -93,7 +105,7 @@ def getCombinedResults(request, _id):
                 combined[name]['name'] = name
                 combined[name]['url'] = item['url']
                 combined[name]['label'] = item['label']
-                combined[name]['similarity_list'] = [0] * 3
+                combined[name]['similarity_list'] = [0] * 5
                 combined[name]['similarity_list'][0] = item['similarity']
 
             for item in sim_rbsd:
@@ -103,7 +115,7 @@ def getCombinedResults(request, _id):
                     combined[name]['name'] = name
                     combined[name]['url'] = item['url']
                     combined[name]['label'] = item['label']
-                    combined[name]['similarity_list'] = [0] * 3
+                    combined[name]['similarity_list'] = [0] * 5
                     combined[name]['similarity_list'][1] = item['similarity']
                 else:
                     combined[name]['similarity_list'][1] = item['similarity']
@@ -115,10 +127,22 @@ def getCombinedResults(request, _id):
                     combined[name]['name'] = name
                     combined[name]['url'] = item['url']
                     combined[name]['label'] = item['label']
-                    combined[name]['similarity_list'] = [0] * 3
+                    combined[name]['similarity_list'] = [0] * 5
                     combined[name]['similarity_list'][2] = item['similarity']
                 else:
                     combined[name]['similarity_list'][2] = item['similarity']
+
+            for item in sim_resnet:
+                name = item['name']
+                if name not in combined:
+                    combined[name] = {}
+                    combined[name]['name'] = name
+                    combined[name]['url'] = item['url']
+                    combined[name]['label'] = item['label']
+                    combined[name]['similarity_list'] = [0] * 5
+                    combined[name]['similarity_list'][3] = item['similarity']
+                else:
+                    combined[name]['similarity_list'][3] = item['similarity']
 
             for item in sim_local:
                 name = item['name']
@@ -127,10 +151,10 @@ def getCombinedResults(request, _id):
                     combined[name]['name'] = name
                     combined[name]['url'] = item['url']
                     combined[name]['label'] = item['label']
-                    combined[name]['similarity_list'] = [0] * 3
-                    combined[name]['similarity_list'][2] = item['similarity']
+                    combined[name]['similarity_list'] = [0] * 5
+                    combined[name]['similarity_list'][4] = item['similarity']
                 else:
-                    combined[name]['similarity_list'][2] = item['similarity']
+                    combined[name]['similarity_list'][4] = item['similarity']
 
             for k, v in combined.items():
                 combined[k]['similarity'] = np.average(combined[k]['similarity_list'])
@@ -145,8 +169,9 @@ def getCombinedResults(request, _id):
                 'cld': sim_cld[:200],
                 'rbsd': sim_rbsd[:200],
                 'segmentation': sim_segmentation[:200],
+                'resnet': sim_resnet[:200],
                 'local': sim_local[:200],
-                'features': ['Combined CLD & RBSD', 'CLD', 'RBSD', 'Segmentation', 'Local']
+                'features': ['Combined', 'CLD', 'RBSD', 'Segmentation', 'Resnet', 'Local']
             })
     except Exception as e:
         print(traceback.print_exc())
@@ -323,20 +348,35 @@ def getVGG16Results(request, _id):
 
 def getResnet20Results(request, _id):
     try:
-        image_instance = QueryImage.objects.get(_id=_id)
-        media_path = os.path.join(settings.BASE_DIR, 'media')
-        image_path = os.path.join(media_path, str(image_instance.file))
-        feature = extract_feature_resnet(image_path)
-        sim = get_similarity_resnet(feature,settings.BASE_DIR)
+        dataset = request.GET.get('dataset', None)
+        if dataset is None:
+            response = JsonResponse({
+                'error': 'Dataset not selected'
+            })
+        elif dataset not in ['cifar', 'pascal']:
+            response = JsonResponse({
+                'error': 'Dataset value incorrect'
+            })
+        else:
+            image_instance = QueryImage.objects.get(_id=_id)
+            media_path = os.path.join(settings.BASE_DIR, 'media')
+            image_path = os.path.join(media_path, str(image_instance.file))
+            if dataset == 'cifar':
+                df = pd.read_pickle(os.path.join(settings.BASE_DIR, 'resnet_features/cifar_resnet_logits.pkl'))
+            elif dataset == 'pascal':
+                df = pd.read_pickle(os.path.join(settings.BASE_DIR, 'resnet_features/deeplab_pascal.pkl'))
 
-        sim.sort(key=lambda x: x['similarity'], reverse=True)
-        print(len(sim))
-        response = JsonResponse({
-            'result': sim[:200],
-            'cld': [],
-            'rbsd': [],
-            'features': ['RESNET', 'CLD', 'RBSD']
-        })
+            descriptor = df.loc[df['file_name'] == image_instance.file].iloc[0, 1].reshape(1, -1)
+            sim_resnet = get_similarity_resnet(descriptor, dataset)
+
+            sim_resnet.sort(key=lambda x: x['similarity'], reverse=True)
+
+            response = JsonResponse({
+                'result': sim_resnet[:200],
+                'cld': [],
+                'rbsd': [],
+                'features': ['VGG', 'CLD', 'RBSD']
+            })
     except Exception as e:
         print(traceback.print_exc())
         response = JsonResponse({
