62351899b929eea304546a72c7fce1cc159cbd50
Tarun Gupta <tarungupta.y12@lnmiit.ac.in>
2020-06-24 01:56:59 +0200

    urls for local text explanations, get random queries, algorithm 2

--- result/urls.py
+++ result/urls.py
@@ -5,7 +5,10 @@ urlpatterns = [
     path('cld/<str:_id>', views.getCLDResults),
     path('rbsd/<str:_id>', views.getRBSDResults),
     path('segmentation/<str:_id>', views.getSegmentationResults),
-    path('<str:_id>', views.getCombinedResults),
     path('vgg/<str:_id>', views.getVGG16Results),
     path('resnet/<str:_id>', views.getResnet20Results),
+    path('local-text/', views.getLocalTextExplanations),
+    path('algo2/<str:_id>', views.getCombinedResultsAlgorithm2),
+    path('queries', views.randomQueries),
+    path('<str:_id>', views.getCombinedResults),
 ]
