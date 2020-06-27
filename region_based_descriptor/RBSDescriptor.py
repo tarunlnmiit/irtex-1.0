4f90ebaea128fe8c636efcd16c236c6c40255c0e
libinjohn26 <51414928+libinjohn26@users.noreply.github.com>
2020-06-27 12:37:28 +0000

    Added explanation

--- region_based_descriptor/RBSDescriptor.py
+++ region_based_descriptor/RBSDescriptor.py
@@ -22,17 +22,28 @@ class NumpyArrayEncoder(JSONEncoder):
 class RBSDescriptor:
     def __init__(self, dataset):
         self.dataset = dataset
+        if self.dataset =='cifar':
+            self.radius = 16
+            self.degree = 16
+        if self.dataset =='pascal':
+            self.radius = 128
+            self.degree = 20
+
         feature_csv_path = os.path.join(settings.BASE_DIR, 'region_based_descriptor')
+
         if self.dataset == 'cifar':
             df = pd.read_pickle(os.path.join(feature_csv_path, 'moments_cifar_pca.pkl'))
+
             #To perform PCA on query image
             df_temp = pd.read_pickle(os.path.join(feature_csv_path, 'moments_cifar.pkl'))
             n_comp = 15
         if self.dataset == 'pascal':
             df = pd.read_pickle(os.path.join(feature_csv_path, 'moments_pascal_pca_updated.pkl'))
+
             # To perform PCA on query image
             df_temp = pd.read_pickle(os.path.join(feature_csv_path, 'moments_pascal_updated.pkl'))
             n_comp = 20
+
         self.df = df
         self.file_name = df['file_name']
         self.moments = df['moments']
@@ -79,21 +90,71 @@ class RBSDescriptor:
 
 # Calculating the zernike moments of query image
     def zernike_moments(self, image):
-        if self.dataset =='cifar':
-            radius = 16
-            degree = 16
-        if self.dataset =='pascal':
-            radius = 128
-            degree = 20
-
-        query_moment = mahotas.features.zernike_moments(image, radius, degree=degree).reshape(1, -1)
+        query_moment = mahotas.features.zernike_moments(image, self.radius, degree=self.degree).reshape(1, -1)
         #To perform PCA on query image
         query_moment = self.pca.transform(query_moment)
         return query_moment
 
-# For textual explanation
-    def textual_explanation(self):
-        return 'text'
+# For Textual and Visual Explanation
+    def explanation(self, image_path, query_path):
+        #media_path = os.path.join(settings.BASE_DIR, 'media')
+        #path_img = os.path.join(media_path, image)
+        #path_query = os.path.join(media_path, query)
+
+        img_array = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
+        query_array = cv2.imread(query_path, cv2.IMREAD_UNCHANGED)
+
+        img_array = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
+        query_array = cv2.cvtColor(query_array, cv2.COLOR_BGR2GRAY)
+        # img = cv2.resize(img, (256, 256), interpolation=cv2.INTER_AREA)
+
+        x_img, y_img = center_of_mass(img_array)
+        x_query, y_query = center_of_mass(query_array)
+        x_img = int(x_img)
+        y_img = int(y_img)
+        x_query = int(x_query)
+        y_query = int(y_query)
+
+        # draw filled circle in white on black background as mask
+        mask_img = np.zeros_like(img_array)
+        mask_query = np.zeros_like(query_array)
+        mask_img = cv2.circle(mask_img, (x_img, y_img), self.radius, (255, 255, 255), -1)
+        mask_query = cv2.circle(mask_query, (x_query, y_query), self.radius, (255, 255, 255), -1)
+
+        # apply mask to image
+        result_img = cv2.bitwise_and(img_array, mask_img)
+        result_query = cv2.bitwise_and(query_array, mask_query)
+
+        result_img = cv2.cvtColor(result_img, cv2.COLOR_GRAY2RGB)
+        result_query = cv2.cvtColor(result_query, cv2.COLOR_GRAY2RGB)
+
+        # save results
+        cv2.imwrite('img_rbsd.png', result_img)
+        cv2.imwrite('query_rbsd.png', result_query)
+
+        list_images = []
+        list_images.append(image)
+        list_images.append(query)
+
+        df_ = self.df[self.df['file_name'].isin(list_images)]
+        moments = df_['moments'].tolist()
+        q_sim = cosine_similarity(moments[0].reshape(1, -1), moments[1].reshape(1, -1))
+        q_sim = [[(sim[0] + 1) / 2] for sim in q_sim]
+
+        score = q_sim[0]
+        score = score[0] * 100
+        score = round(score, 2)
+        text = 'Below regions were compared to get the similairty score. The similarity between this two region is ' \
+               + str(score) + '%'
+
+        textual = []
+        textual.append(text)
+
+        visual = []
+        visual.append('img_rbsd.png')
+        visual.append('query_rbsd.png')
+
+        return textual, visual
 
 # Pre-processing the query image so as to match extracted features
     def image_preprocessing(self, image):
