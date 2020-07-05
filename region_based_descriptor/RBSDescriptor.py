d0088b02ab6f03626f1fc8b71f2ac3dc6590481e
Libin Kutty <libin.kutty@st.ovgu.de>
2020-07-05 12:48:38 +0000

    Changed textual explanation

--- region_based_descriptor/RBSDescriptor.py
+++ region_based_descriptor/RBSDescriptor.py
@@ -147,7 +147,9 @@ class RBSDescriptor:
         score = q_sim[0]
         score = score[0] * 100
         score = round(score, 2)
-        text = 'Below regions were compared to get the similarity score. The similarity between these two region is ' + str(score) + '%'
+
+        text1 = 'The images points out the region of interest which was considered to find similarity. '
+        text2 = 'The similarity between these two region is ' + str(score) + '%'
 
         # textual = []
         # textual.append(text)
@@ -157,7 +159,7 @@ class RBSDescriptor:
         # visual.append('query_rbsd.png')
 
         explanation = {}
-        explanation['text'] = [text]
+        explanation['text'] = [text1, text2]
 
         explanation['images'] = [{'name': 'Query Image', 'url': '/media/rbsd/{}_rbsd.png'.format(query_image_name.split('.')[0])},
                              {'name': 'Result Image',
